4d6fb08e052c790ba77396e0ffc53a192b3ca0d4
Mark McArthey <mcarthey@gmail.com>
2025-11-14 18:43:06 -0600

    Added migration for rooms

---
+++ ConsoleRpgEntities/Migrations/20251115004122_AddRoomTable.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleRpgEntities.Migrations
+{
+    public partial class AddRoomTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Players",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Monsters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Value",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Weight",
+                table: "Items",
+                type: "decimal(3,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    X = table.Column<int>(type: "int", nullable: false),
+                    Y = table.Column<int>(type: "int", nullable: false),
+                    NorthRoomId = table.Column<int>(type: "int", nullable: true),
+                    SouthRoomId = table.Column<int>(type: "int", nullable: true),
+                    EastRoomId = table.Column<int>(type: "int", nullable: true),
+                    WestRoomId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Rooms_EastRoomId",
+                        column: x => x.EastRoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Rooms_NorthRoomId",
+                        column: x => x.NorthRoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Rooms_SouthRoomId",
+                        column: x => x.SouthRoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Rooms_WestRoomId",
+                        column: x => x.WestRoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_RoomId",
+                table: "Players",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Monsters_RoomId",
+                table: "Monsters",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_EastRoomId",
+                table: "Rooms",
+                column: "EastRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_NorthRoomId",
+                table: "Rooms",
+                column: "NorthRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_SouthRoomId",
+                table: "Rooms",
+                column: "SouthRoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_WestRoomId",
+                table: "Rooms",
+                column: "WestRoomId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Monsters_Rooms_RoomId",
+                table: "Monsters",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Players_Rooms_RoomId",
+                table: "Players",
+                column: "RoomId",
+                principalTable: "Rooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Monsters_Rooms_RoomId",
+                table: "Monsters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Players_Rooms_RoomId",
+                table: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Players_RoomId",
+                table: "Players");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Monsters_RoomId",
+                table: "Monsters");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Monsters");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Items");
+        }
+    }
+}
